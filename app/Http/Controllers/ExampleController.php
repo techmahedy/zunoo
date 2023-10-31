57dbdd56a15e953deb55cb40322213612a7a4900
Arif Hoque <arif@sulaco-tec.com>
2023-10-31 22:18:39 +0600

    up

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -2,19 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use App\Core\Request;
 use App\Core\Controllers\Controller;
 
 class ExampleController extends Controller
 {
-    public function __construct(User $user)
-    {
-        # code...
-    }
-
-    public function index(Request $request)
-    {
-        return "wow";
-    }
 }
