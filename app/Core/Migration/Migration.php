8928c55d8a887bb4068e144b6322753e0c2bf6ce
techmahedy <mahedy150101@gmail.com>
2023-11-11 20:48:47 +0600

    migration support added

---
+++ app/Core/Migration/Migration.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Core\Migration;
+
+use Phinx\Migration\AbstractMigration;
+
+class Migration extends AbstractMigration
+{
+}
---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'paths' => [
+        'migrations' => './database/migrations',
+        'seeds' => './database/seeds',
+    ],
+    'migration_base_class' => 'App\Core\Migration\Migration',
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'laravel10' => [
+            'adapter' => 'mysql',
+            'host' => 'localhost',
+            'name' => 'laravel10',
+            'user' => 'mahedi',
+            'pass' => '123',
+            'port' => '3306'
+        ]
+    ]
+];
