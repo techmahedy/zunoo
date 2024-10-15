658175e8b41467a3190a5ef24c21fd61f9cf07ec
Arif Hoque <arif@sulaco-tec.com>
2024-10-15 21:50:20 +0600

    version updated 1.0.2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Zuno-{{ \Zuno\Application::VERSION }}</title>
+        <title>Zuno</title>
         <style>
             @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css');
 
