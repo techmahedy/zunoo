5d9c3c9c0182f083a9ef071b6731277c1f7fdbac
Arif Hoque <arif@sulaco-tec.com>
2025-04-02 11:48:13 +0600

    v6.4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,23 @@
-@extends('layouts.app')
-@section('title') Welcome
-@section('content')
-    <div class="container d-flex justify-content-center align-items-center" style="height: 65vh;">
-        <div class="content text-center">
-            <p style="font-weight: 700; font-size:20px">
-                Thank you for choosing Zuno {{ \Zuno\Application::VERSION }}. Build
-                something amazing!</p>
-            <div class="buttons">
-                <a href="https://github.com/techmahedy/zunoo" class="btn btn-light" style="background: #D3D4D5">Github</a>
-                <a href="https://github.com/techmahedy/zunoo" class="btn btn-light"
-                    style="background: #D3D4D5">Documentation</a>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-KyZXEJ6Jtpu9ay0w7OCk8nR2HfejF2pxp5Z2t8pqLwzG3xxN3XkL9Pso8toftx2j" crossorigin="anonymous">
+    </head>
+    <body>
+        <div style="display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0;">
+            <div style="text-align: center;">
+                <p>Thank you for choosing Zuno {{ \Zuno\Application::VERSION }}. Build something amazing!</p>
+                <div>
+                    <a href="https://github.com/techmahedy/zunoo" class="btn btn-light"
+                        style="padding: 10px 20px; margin: 5px;">Github</a>
+                    <a href="https://github.com/techmahedy/zunoo" class="btn btn-light"
+                        style="padding: 10px 20px; margin: 5px;">Documentation</a>
+                </div>
             </div>
         </div>
-    </div>
-@endsection
+    </body>
+</html>
